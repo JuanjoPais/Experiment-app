b4c648c629b407dcb5668182970c370dd5b25e42
Juan Pais <jjosepais@gmail.com>
2022-09-25 21:46:49 -0300

    sumo una la función nombre

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
-let nombre = prompt("Ingresa tu nombre");
 
-let apellido = prompt("Ingresa tu apellido");
 
-alert(`Bienvenid@ ${nombre} ${apellido}.`);
+function nombre (){
+    let nombre = prompt("Ingresa tu nombre");
+
+    let apellido = prompt("Ingresa tu apellido");
+
+    let nombreCompleto = `${nombre} ${apellido}`;
+
+    return nombreCompleto;
+}
+
+nombre();
 
 //Hago un algoritmo condicional, y dentro de una de las condiciones incluyo un ciclo.
 
-let accion = prompt(`${nombre}, tenés un nuevo experimento para cargar (tipeá ´exp´)?. Querés sumar datos a un experimento actual (tipeá ´datos´)?. Si querés navegar el sitio, elegí cancelar.`)
+let accion = prompt(`Tenés un nuevo experimento para cargar (tipeá ´exp´)?. Querés sumar datos a un experimento actual (tipeá ´datos´)?. Si querés navegar el sitio, elegí cancelar.`)
 
-if (accion == "exp") {
-    let nombreExpe = prompt("Cómo se llama el experimento?");
-    let equipo = prompt("A qué equipo pertenece?");
-    let tiempoExpe = parseInt(prompt("Cuántos días estimás que dure?"))
+if (accion == "exp") {   
+        
+        let nombreExpe = prompt("Cómo se llama el experimento?")     
+        let equipo = prompt("A qué equipo pertenece?");
 
     alert(`Se sumó el experimento ${nombreExpe} al equipo ${equipo}. Podrás modificar sus condiciones desde "Ver tareas"`)
 
 } else if (accion == "datos") {
     let dia = 1
-    let conteo = parseInt(prompt(`Ingresá el conteo de células del día ${dia}. Tipeá ´000´ para salir.`));
+    let conteo = parseInt(prompt(`Ingresá el conteo de células del día ${dia}. Tipeá ´000´ para salir.`));    
 
     while (conteo != 000) {
         dia++;
         conteo = parseInt(prompt(`Ingresá el conteo de células del día ${dia}. Tipeá ´000´ para salir.`));
 
-    };
+    };   
 
     alert("Los datos se sumaron a tu experimento en curso" )
 
