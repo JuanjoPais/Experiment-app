9965b939e3b9bd69093c1ea62d6472c001031aa9
Juan Pais <jjosepais@gmail.com>
2022-10-10 15:39:04 -0300

    Empiezo con DOM

--- javascript/main.js
+++ javascript/main.js
@@ -1,78 +1,128 @@
-
-
-function nombre (){
-    let nombre = prompt("Ingresa tu nombre");
-
-    let apellido = prompt("Ingresa tu apellido");
-
-    let nombreCompleto = `${nombre} ${apellido}`;
-
-    return nombreCompleto;
-}
-
-//nombre();
-
-//Hago un algoritmo condicional, y dentro de una de las condiciones incluyo un ciclo.
-
-//let accion = prompt(`Tenés un nuevo experimento para cargar (tipeá ´exp´)?. Querés sumar datos a un experimento actual (tipeá ´datos´)?. Si querés navegar el sitio, elegí cancelar.`)
-
-if (accion == "exp") {   
-        
-        let nombreExpe = prompt("Cómo se llama el experimento?")     
-        let equipo = prompt("A qué equipo pertenece?");
-
-    alert(`Se sumó el experimento ${nombreExpe} al equipo ${equipo}. Podrás modificar sus condiciones desde "Mis experimentos"`)
-
-} else if (accion == "datos") {
-    let dia = 1
-    let conteo = parseInt(prompt(`Ingresá el conteo de células del día ${dia}. Tipeá ´000´ para salir.`));    
-
-    while (conteo != 000) {
-        dia++;
-        conteo = parseInt(prompt(`Ingresá el conteo de células del día ${dia}. Tipeá ´000´ para salir.`));
-
-    };   
-
-    alert("Los datos se sumaron a tu experimento en curso" )
-
-} else {
-    alert("Si buscás otra acción, navegá el sitio")
-};
-
 // defino arrays
 
-const equipos = [tissue, stem, eco];
+const equipos = ["tissue", "stem", "eco"];
 
 const expesCreados = [];
+
+const usuarios = [];
 
 
 
 // defino la cantidad inicial de medio de cultivo
 
-let cantidadMedioCultivoMl= 100;
+let stockMedioCultivoMl = 100;
+
+
 
 // armo el constructor de objetos
 
 
 class Experimento {
-    constructor (nombre, equipo, duracion, consumoMedioCultivo){
-        this.nombre = nombre;
-        this.equipo= equipo;
-        this.duracion= duracion;
-        this.consumoMedioCultivo= consumoMedioCultivo;
+    constructor(nombreExp, equipo, duracion, consumoMedioCultivo) {
+        this.nombreExp = nombreExp;
+        this.equipo = equipo;
+        this.duracion = duracion;
+        this.consumoMedioCultivo = consumoMedioCultivo;
     }
-        
+    restarMedioCultivo() {
+        stockMedioCultivoMl -= this.consumoMedioCultivo;
+        alert(`Se actualizó el stock del medio de cultivo. Ahora quedan ${stockMedioCultivoMl} ml.`);
+        return stockMedioCultivoMl;
+
+    }
+
 }
 
-const crearExperimento = () =>{
-    let nombreExp 
-    let seleccionEquipo 
-    let seleccionDuracion
-    let seleccionConsumoMedioCultivo 
+const crearExperimento = () => {
+    let nombreExp = document.getElementById("nombreExp").value;
+    let seleccionEquipo = document.getElementById("equipoExp").value;
+    let seleccionDuracion = document.getElementById("diasExp").value;
+    let seleccionConsumoMedioCultivo = document.getElementById("consumoMedioCultivo").value;
 
     const experimento = new Experimento(nombreExp, seleccionEquipo, seleccionDuracion, seleccionConsumoMedioCultivo);
+
+    experimento.restarMedioCultivo();
 
     expesCreados.push(experimento);
     return experimento;
 }
 
+
+
+
+if (stockMedioCultivoMl < 10) {
+    alert(`Reponer medio de cultivo. Sólo quedan ${stockMedioCultivoMl} ml.`);
+}
+
+const botonNuevoExp = document.getElementById("botonNuevoExp");
+
+botonNuevoExp.addEventListener("click", crearExperimento);
+
+
+
+//defino un funcion para quitar los experimentos que vayan terminando
+
+const quitarExpes = () => {
+    expesCreados.pop();
+    return expesCreados;
+};
+
+
+//Armo objeto y array para almacenar usuarios
+
+const listadoUsuarios = [];
+
+
+class Usuarios {
+    constructor(nombre, apellido, nombreUsuario, constrasenia) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nombreUsuario = nombreUsuario;
+        this.constrasenia = constrasenia;
+    }
+}
+
+const crearUsuario = () => {
+    let nombre = document.getElementById("nombreUsuario").value;
+    let apellido = document.getElementById("apellidoUsuario").value;
+    let nombreUsuario = document.getElementById("usuario").value;
+    let constrasenia = document.getElementById("contraseniaUsuario").value;
+
+    const nuevoUsuario = new Usuarios(nombre.toLowerCase(), apellido.toLowerCase(), nombreUsuario.toLowerCase(), constrasenia);
+
+    listadoUsuarios.push(nuevoUsuario);
+    return listadoUsuarios;
+}
+
+
+//creo un div con un reactivo extra
+
+let nuevoReactivo = document.getElementById("formRadioDefault");
+
+const mostrarSumarReactivo = () => {
+
+    let divNuevoReactivo = document.createElement("div");
+
+    divNuevoReactivo.classList.add("divNuevoReactivo");
+
+    divNuevoReactivo.innerHTML = `
+          <form>
+          <input class ="m-2" type="text" placeholder="Reactivo"> 
+          <input class ="m-2" type="text" placeholder="Marca y número de lote"> 
+          <input class ="m-2" type="text" placeholder="Cantidad a utlizar"> 
+          <button  id="botonIncluirReactivo" class ="m-2">Incluir Reactivo</button>
+          </form>`;
+
+    document.getElementById("nuevoReactivo").append(divNuevoReactivo);
+
+}
+
+nuevoReactivo.addEventListener("click", mostrarSumarReactivo);
+
+//hago preventDefault en los formularios
+
+let formulario = document.getElementById("formulario");
+
+formulario.addEventListener("click", (e) => {
+    e.preventDefault()
+});
